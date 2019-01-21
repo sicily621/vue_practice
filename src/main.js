285e881be4e3174bfa4de33627da5301c59780ee
sicily <2729469736@qq.com>
2019-01-21 14:47:22 +0800

    git@github.com:sicily621/vue_practice.git

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue';
 import app from './App.vue';
+import vueRouter from 'vue-router';
+Vue.use(vueRouter);
 import { Header } from 'mint-ui';
 Vue.component(Header.name, Header);
+import router from './router.js';
 import './lib/mui/css/mui.min.css';
+import './lib/mui/css/icons-extra.css';
 var vm = new Vue({
     el:'#app',
-    render:c=>c(app)
+    render:c=>c(app),
+    router
 })
--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router';
 var router = new VueRouter({
     routes:[
         
-    ]
+    ],
+    linkActiveClass:'mui-active'
 })
 export default router;
